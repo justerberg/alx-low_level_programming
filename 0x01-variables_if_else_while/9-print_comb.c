b4b5f9a0d14f8bb513913a3398041c6523e9af2c
justerberg <justerberg@gmail.com>
2023-02-17 09:23:18 -0800

    9-print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include "betty.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the numbers 0 to 9 separated by commas and spaces.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
+        for (num = 0; num <= 9; num++)
+        {
+                putchar((num % 10) + '0');
+                if (num == 9)
+                        continue;
 
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
+                putchar(',');
+                putchar(' ');
+        }
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
