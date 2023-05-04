14866aa8e037aba1b2f49c790fbfe8e42e7e4b5d
justerberg <justerberg@gmail.com>
2023-05-05 00:42:45 +0100

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * get_bit - returns the value of a bit at a given index
- * @n: the number to check bits of
- * @index: the index of the bit to return
+ * get_bit - Returns the value of a bit at a given index
+ * @n: The num to check bits of
+ * @index: The index of the bit to return
  *
- * Return: the value of the bit at index, or -1 if an error occurred
+ * Return: -1 if an error occurred, or the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- * flip_bits - returns the number of bits needed to flip to convert n to m
- * @n: first number
- * @m: second number
+ * flip_bits - Returns the number of bits needed to flip to convert n to m
+ * @n: First number
+ * @m: Second number
  *
- * Return: the number of bits needed to flip to convert n to m
+ * Return: The number of bits needed to flip to convert n to m
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int difference, result;
-	unsigned int bit_count, i;
-	bit_count = 0;
-	result = 1;
-	difference = n ^ m;
-	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
-	{
-		if (result == (difference & result))
-			bit_count++;
-		result <<= 1;
-	}
-	return (bit_count);
+unsigned long int difference, result;
+unsigned int bit_count, i;
+bit_count = 0;
+result = 1;
+difference = n ^ m;
+for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+{
+if (result == (difference & result))
+bit_count++;
+result <<= 1;
+}
+
+return (bit_count);
 }
